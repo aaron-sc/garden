ac1f527963b2e9f77b14ed0b5f1c8249469b7a2c
aaron-sc <aaron.santacruz03@gmail.com>
2020-04-05 16:43:09 -0600

    Added install tool

---
+++ garden.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ..
+if[! -d "garden"]; then
+	sudo git clone https://github.com/aaron-sc/garden.git
+	echo "Downloaded! Installing tools!"
+	sudo python3 pip install -r garden/requirements.txt
+	echo "Tools downloaded! Run sudo python3 garden.py! Run this tool again to update!"
+else
+	echo "Updating!"
+	sudo rm -rf garden
+	sudo git clone https://github.com/aaron-sc/garden.git
+	echo ""
+	echo "Complete!"
+fi
