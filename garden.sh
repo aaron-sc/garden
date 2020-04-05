2c5fbe531ae16a2aedc214d5e50488aa318ebfa8
aaron-sc <aaron.santacruz03@gmail.com>
2020-04-05 16:54:53 -0600

    Update garden.sh

--- garden.sh
+++ garden.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-cd ..
-if [ ! -d "garden" ]; then
-	sudo git clone https://github.com/aaron-sc/garden.git
-	echo "Downloaded! Installing tools!"
-	sudo python3 pip install -r garden/requirements.txt
-	echo "Tools downloaded! Run sudo python3 garden.py! Run this tool again to update!"
-else
-	echo "Updating!"
-	sudo rm -rf garden
-	sudo git clone https://github.com/aaron-sc/garden.git
-	echo ""
-	echo "Complete!"
-fi
+cd
+sudo rm -rf garden
+sudo git clone https://github.com/aaron-sc/garden.git
+sudo python3 pip install -r requirements.txt
+echo "Done! Run this script again to update!"
