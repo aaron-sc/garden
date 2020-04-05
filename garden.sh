8a2780eba9b7436c14c7905928f3650126f422d7
aaron-sc <aaron.santacruz03@gmail.com>
2020-04-05 16:59:07 -0600

    Update for running

--- garden.sh
+++ garden.sh
@@ -2,5 +2,5 @@
 cd
 sudo rm -rf garden
 sudo git clone https://github.com/aaron-sc/garden.git
-sudo python3 pip install -r requirements.txt
+sudo pip3 install -r ~/garden/requirements.txt
 echo "Done! Run this script again to update!"
